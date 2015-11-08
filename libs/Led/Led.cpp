29ba5f7507afd9f6e667bde110194715ff3ce2f6
Pascal Frehner <frehnerp@gmail.com>
2015-11-08 14:39:26 +0100

    first library added

---
+++ libs/Led/Led.cpp
@@ -0,0 +1,18 @@
+/*
+ * Led.cpp
+ *
+ *  Created on: 08.11.2015
+ *      Author: frehnerp
+ */
+
+#include "Led.h"
+
+Led::Led() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Led::~Led() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ libs/Led/Led.h
@@ -0,0 +1,17 @@
+/*
+ * Led.h
+ *
+ *  Created on: 08.11.2015
+ *      Author: frehnerp
+ */
+
+#ifndef LED_LED_H_
+#define LED_LED_H_
+
+class Led {
+public:
+	Led();
+	virtual ~Led();
+};
+
+#endif /* LED_LED_H_ */
